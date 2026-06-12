28e3a870aa4cd470d198faa4ab400fdb29d9cc99
LATENT STRUCTURE <latent-structure@example.com>
2026-06-12 15:20:08 +0200

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/-LATENT_STRUCTURE_portfolio_project/',
   plugins: [react()],
 });
